2bfc1f5ecef1db8b078e8d06ea4540c813e7ad47
ravikant <ravikantyadav3243@gmail.com>
2026-08-13 15:51:27 +0530

    setups the psycopg

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,50 @@
-from fastapi import FastAPI
+from time import time
+from typing import Optional
+
+from fastapi import Body, FastAPI, HTTPException, Response, status
+import psycopg
+from pydantic import BaseModel
+
+
+class Post(BaseModel):
+    title:str
+    content:str
+    isPublished: Optional[bool]=False
 
 app=FastAPI()
 
+
+while True:
+    try:
+        connection= psycopg.connect(
+        host='localhost',
+        dbname='fastapi',
+        user='postgres',
+        password='PostgreSQL',
+        port=5432
+
+         ) ;
+        cursor=connection.cursor();
+        print("connection succesful")
+        print("connection:",connection);
+        print("cursor:",cursor)  
+        break    
+
+    except Exception as error:
+        print("Failed to connect")
+        print("Error is: ",error)
+        time.sleep(2)
+
+
+
 @app.get("/")
-def hello_fastapi():
-    return {"message": "Hello FastAPI!"}
+def root():
+    return {"message":"hello fastapi"}
+
+
+
+@app.get("/posts")
+def get_posts():
+    methodCall=cursor.execute("SELECT * FROM posts")
+    posts=methodCall.fetchall()
+    return {"data":posts}
